d8a08661d32481691602d3d1250044c4b509a253
Iryna0810 <irinavakulenko2000@gmail.com>
2023-01-03 13:14:03 +0200

    js start

---
+++ js/01-vars.js
@@ -0,0 +1,15 @@
+const age = 10;
+const totalPrice = 200.74;
+const userName = 'Chelsy';
+const message = 'Добро пожаловать';
+const isOpen = true;
+const shouldConfirm = false;
+
+console.log('Price', totalPrice, message);
+const type = typeof isOpen;
+console.log(type)
+
+
+// console.log('Before');
+// alert('gjreiot0wer9');
+// console.log('After')
