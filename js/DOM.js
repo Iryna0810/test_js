2fa1648698a7032c58f2cc2497e218c85a100b7e
Rew <andriyvak1233@gmail.com>
2023-02-26 17:34:40 +0200

    101

--- js/DOM.js
+++ js/DOM.js
@@ -146,6 +146,31 @@ list.insertAdjacentHTML("beforeend", markup);
 list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
 
 
+const singleBtn = document.querySelector("#single");
+
+const handleClick = () => {
+  console.log("click event listener callback");
+};
+
+singleBtn.addEventListener("click", handleClick);
+
+// ===============================================
+const multiBtn = document.querySelector("#multiple");
+
+const firstCallback = () => {
+  console.log("First callback!");
+};
+const secondCallback = () => {
+  console.log("Second callback!");
+};
+const thirdCallback = () => {
+  console.log("Third callback!");
+};
+
+multiBtn.addEventListener("click", firstCallback);
+multiBtn.addEventListener("click", secondCallback);
+multiBtn.addEventListener("click", thirdCallback);
+
 
 
 
