e73468f74b37ebc0f05591e833c85c8379622b56
Rew <irinavakulenko2000@gmail.com>
2023-03-05 07:18:16 +0200

    100

--- gallery.js
+++ gallery.js
@@ -1,6 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-
 console.log(galleryItems);
-// const titleEl = document.querySelector('.title');
 
+
+const makeGalleryCard = ({ preview, original, description }) => {
+    return `<li class='gallery-item'>
+    <a href="#">
+    <img src="${preview}" alt="${description}">
+    </a>
+    </li>`;   
+}
+const imagesList = document.querySelector('.images-list');
+console.log(imagesList);
+
+const markup = galleryItems.map((data) => makeGalleryCard(data)).join("");
+
+imagesList.insertAdjacentHTML('afterbegin', markup);
+
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
 
