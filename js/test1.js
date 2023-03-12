98ff7354495ea4e046df4dd010acf0119fba8fc0
Rew <irinavakulenko2000@gmail.com>
2023-03-12 17:27:23 +0200

    Update test1.js

--- js/test1.js
+++ js/test1.js
@@ -11,3 +11,22 @@ console.log(firstListItem);
 
 const listItems = list.children;
 console.log(listItems);
+
+const user = {
+    firstName: "Iryna",
+    age: 40
+};
+
+localStorage.setItem('user', JSON.stringify(user));
+
+console.log(JSON.parse(localStorage.getItem('user')));
+console.log(localStorage.length);
+
+localStorage.setItem('luckyNumber', JSON.stringify('500'));
+
+// localStorage.removeItem('user');
+
+// localStorage.clear();
+
+
+console.log(JSON.parse(localStorage.getItem('user')));
